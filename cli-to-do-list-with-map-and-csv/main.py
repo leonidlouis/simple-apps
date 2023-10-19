66a9552c1baca319316658ae95421c360ce41579
Louis Leonid <louisleonid325@gmail.com>
2023-10-19 14:05:02 +0700

    add arguments to cli application to optionally read and write from file

--- cli-to-do-list-with-map-and-csv/main.py
+++ cli-to-do-list-with-map-and-csv/main.py
@@ -1,10 +1,15 @@
+import argparse
 import csv
+import sys
 
 class ToDo:
-    def __init__(self):
+    def __init__(self, file_name=""):
         self.to_do = {}
         self.task_id = 0
-        self.file_name = "tasks.csv"
+        self.file_name = file_name
+
+        if self.file_name:
+            self.load_from_csv()
 
     def get_task_by_id(self, id):
         id = int(id)
@@ -34,6 +39,7 @@ class ToDo:
                     }
                     self.task_id = max(self.task_id, task_id) # since we don't know the ordering of the csv
         except FileNotFoundError:
+            print(f"File {self.file_name} is not found, skipping loading from csv")
             pass
     
     def mark_as_done(self, id):
@@ -62,10 +68,9 @@ class ToDo:
         return f"Task of ID {id} is not found"
 
     def list(self):
-        # return self.to_do
         return list(self.to_do.values())
 
-    def execute(self, command):
+    def execute(self, command, should_write_to_file=False):
         parts = command.split()
         operation = parts[0]
 
@@ -93,7 +98,7 @@ class ToDo:
             value = ' '.join(parts[1:])
             res = operation_map[operation](value)
 
-        if operation in ["add", "done", "delete", "update"]:
+        if should_write_to_file and operation in ["add", "done", "delete", "update"]:
             self.save_to_csv()
 
         return res
@@ -106,8 +111,22 @@ class ToDo:
                 writer.writerow([task["id"], task["name"], task["is_done"]])
 
 def main():
-    to_do_app = ToDo()
-    to_do_app.load_from_csv()
+    parser = argparse.ArgumentParser("To Do List")
+    parser.add_argument("--read-from-file", type=str, help="if set, it will initially read from that file")
+    parser.add_argument("--write-to-file", action="store_true", help="if used, it will write back to file every changes")
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1: #means no argument is provided
+        print("You've provided no arguments and have entered interactive mode, please put in the required information")
+        read_from_file = input("Input the filepath of your CSV (default: None): ").strip()
+        args.read_from_file = read_from_file if read_from_file else args.read_from_file
+
+        if args.read_from_file:
+            write_to_file = input("If set, application will write to file (true/false): ").lower() == "true"
+            args.write_to_file = write_to_file
+
+    to_do_app = ToDo(file_name=args.read_from_file)
 
     while True:
         command = input("> ")
@@ -115,7 +134,7 @@ def main():
         if command == "exit":
             break
 
-        print(to_do_app.execute(command))
+        print(to_do_app.execute(command, args.write_to_file))
 
 if __name__ == "__main__":
     main()
